a31babdb5436a9a509e0296c69980bedc6212fdc
Samuel Gomez <samgj_18@hotmail.com>
2025-02-03 20:38:05 +0000

    fix: removing tracing of event access

--- api/src/logic/mod.rs
+++ api/src/logic/mod.rs
@@ -121,7 +121,6 @@ where
     T: RequestExt<Output = U> + HookExt<U> + PublicExt<U> + 'static,
     U: Serialize + DeserializeOwned + Unpin + Sync + Send + Debug + 'static,
 {
-    tracing::info!("EventAccess: {:?}", access);
     let output = access
         .map(|e| payload.access(e.0))
         .unwrap_or_else(|| payload.from())
