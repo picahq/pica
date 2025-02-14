be62400c2637cf98f97e60da123a754e20076237
Paul K. <paul@paulkr.com>
2025-02-14 17:54:52 -0500

    feat: add github oauth endpoints

---
+++ oauth/src/connections/github/refresh.ts
@@ -0,0 +1,19 @@
+import { DataObject, OAuthResponse } from '../../lib/types';
+
+export const refresh = async ({ body }: DataObject): Promise<OAuthResponse> => {
+    try {
+        const {
+            OAUTH_METADATA: { accessToken, refreshToken, tokenType, meta },
+        } = body;
+
+        return {
+            accessToken,
+            refreshToken,
+            expiresIn: 2147483647, // Set max expiration
+            tokenType,
+            meta,
+        };
+    } catch (error) {
+        throw new Error(`Error fetching refresh token for GitHub: ${error}`);
+    }
+}; 
