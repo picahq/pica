cbc0123eb2eb4d19f273d2201652578989b8c2b1
Samuel Gomez <samgj_18@hotmail.com>
2025-02-19 23:19:49 +0000

    chore: send platform info in knowledge response

--- api/src/logic/knowledge.rs
+++ api/src/logic/knowledge.rs
@@ -2,7 +2,7 @@ use super::{read_without_key, HookExt, PublicExt, RequestExt};
 use crate::server::{AppState, AppStores};
 use axum::{routing::get, Router};
 use bson::doc;
-use entities::{record_metadata::RecordMetadata, Id, MongoStore};
+use entities::{connection_model_definition::PlatformInfo, record_metadata::RecordMetadata, Id, MongoStore};
 use fake::Dummy;
 use serde::{Deserialize, Serialize};
 use std::sync::Arc;
@@ -22,6 +22,9 @@ pub struct Knowledge {
     pub title: String,
     pub path: String,
     pub knowledge: Option<String>,
+    #[serde(flatten)]
+    pub platform_info: PlatformInfo,
+    pub base_url: Option<String>,
     #[serde(flatten)]
     pub metadata: RecordMetadata,
 }
