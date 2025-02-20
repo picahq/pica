b26254874327c2ae45c21355ab59d9106f0e4de8
Samuel Gomez <samgj_18@hotmail.com>
2025-02-20 19:59:20 +0000

    feat: adding track endpoint

---
+++ api/src/logic/tracker.rs
@@ -0,0 +1,17 @@
+use crate::{domain::track::TrackedMetric, server::AppState};
+use axum::{extract::State, routing::post, Json, Router};
+use entities::{PicaError, Unit};
+use std::sync::Arc;
+
+pub fn get_router() -> Router<Arc<AppState>> {
+    Router::new().route("/", post(create_tracking))
+}
+
+pub async fn create_tracking(
+    state: State<Arc<AppState>>,
+    Json(payload): Json<TrackedMetric>,
+) -> Result<Unit, PicaError> {
+    state.tracker_client.track_event(payload).await?;
+
+    Ok(())
+}
