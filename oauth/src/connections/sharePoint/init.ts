6f44aaa89f16354f5600cb9401d6e3c46b0b0990
Siddharth Bhansali <siddharthbhansali13@hotmail.com>
2025-05-20 18:17:35 +0530

    fix: share point oauth configuration

---
+++ oauth/src/connections/sharePoint/init.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { DataObject, OAuthResponse } from '../../lib/types';
+
+export const init = async ({ body }: DataObject): Promise<OAuthResponse> => {
+    try {
+        const {
+            clientId: client_id,
+            clientSecret: client_secret,
+            metadata: { code, redirectUri: redirect_uri } = {},
+        } = body;
+
+        const data = {
+            client_id,
+            client_secret,
+            code,
+            redirect_uri,
+            scope: 'offline_access Sites.ReadWrite.All Sites.Manage.All Sites.FullControl.All',
+            grant_type: 'authorization_code',
+        };
+
+        const response = await axios({
+            url: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            data,
+        });
+
+        const {
+            data: {
+                access_token: accessToken,
+                refresh_token: refreshToken,
+                token_type: tokenType,
+                expires_in: expiresIn,
+                scope,
+            },
+        } = response;
+
+        return {
+            accessToken,
+            refreshToken,
+            expiresIn,
+            tokenType,
+            meta: {
+                scope,
+            },
+        };
+    } catch (error) {
+        throw new Error(`Error fetching access token for SharePoint: ${error}`);
+    }
+};
